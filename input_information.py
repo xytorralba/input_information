faeac4c1d27384ceea95f0f29f17302579b992a6
Xyrene Jade Torralba <182231457+xytorralba@users.noreply.github.com>
2024-10-23 17:21:51 +0800

    The user can now input their information.

--- input_information.py
+++ input_information.py
@@ -1,7 +1,24 @@
+Profile = {}
+
 #Loop1: Asks the user to input their information
+while True:
+    try:
+        name = print(input("Name: "))
+        age = print(int(input("Age: ")))
+
+        Profile = {
+            "name" : name,
+            "age" : age
+        }
+
+        print(Profile["name"])
+        print(Profile["age"])
+        
+        #Stops Loop1
+        break
+    except:
+        print("Please input valid name or age.")
 
 #Loop2: Allows the user to auto-retry if input is invalid
-
-#Stops Loop1
 
 #Stops Loop2
